6cb9d002996ece01f5a3d6b7cbf84cb8fd62bd38
grey949 <gary.rey@gmail.com>
2015-10-27 13:46:59 -0700

    Testing

--- www/js/index.js
+++ www/js/index.js
@@ -33,6 +33,8 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar = NO;
         app.receivedEvent('deviceready');
     },
     // Update DOM on a Received Event
